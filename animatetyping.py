0344307ae34f0b3f6a5f5eca233b678828be36fd
chandrasreasgop <chandrasreasgop@gmail.com>
2022-09-29 09:08:02 +0530

    ASCII Art added to animatetyping.py(using Pyfiglet)

--- animatetyping.py
+++ animatetyping.py
@@ -1,8 +1,11 @@
+import pyfiglet
 from sys import exit, stdout
 from random import randint
 from time import sleep
 
 def main():
+    print(pyfiglet.figlet_format("Animate Typing"))
+    print("Welcome to the Animate Typing Terminal Utility!")
     data = input("Enter what you want the computer to type: ")
     Comptype(data)
 
@@ -13,11 +16,12 @@ def Comptype(userinput):
         exit()
     else:
         for i in range(length_of_user_input):
-            random_time_gap = randint(1,5)
+            random_time_gap = randint(1,6)
             stdout.write(f"\r{userinput[:i+1]}")
-            sleep(0.06*random_time_gap)
+            sleep(0.05*random_time_gap)
         print()
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
