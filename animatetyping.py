41e9451a61e454bbc3ad43818f6ed1ccec6a88df
chandrasreasgop <chandrasreasgop@gmail.com>
2022-09-29 08:11:07 +0530

    .\animatetyping.py added

---
+++ animatetyping.py
@@ -0,0 +1,23 @@
+from sys import exit, stdout
+from random import randint
+from time import sleep
+
+def main():
+    data = input("Enter what you want the computer to type: ")
+    Comptype(data)
+
+
+def Comptype(userinput):
+    length_of_user_input = len(userinput)
+    if userinput.replace(" ","").lower() == 'exit': 
+        exit()
+    else:
+        for i in range(length_of_user_input):
+            random_time_gap = randint(1,5)
+            stdout.write(f"\r{userinput[:i+1]}")
+            sleep(0.06*random_time_gap)
+        print()
+
+
+if __name__ == '__main__':
+    main()
